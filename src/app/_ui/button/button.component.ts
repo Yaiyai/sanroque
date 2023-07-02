0e1aa07d5f45d3bf79926687912b0d4e53e4c346
yaiyai <yaiza.delrio@clupik.com>
2023-07-02 13:41:59 +0200

    filter styles

--- src/app/_ui/button/button.component.ts
+++ src/app/_ui/button/button.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class ButtonComponent {
   @Input() label: string = '';
-  @Input() class: 'main' | 'outline' | 'go' | 'link' = 'main';
+  @Input() class: 'main' | 'outline' | 'go' | 'link' | 'gray' = 'main';
   constructor() { }
 
   ngOnInit() {
