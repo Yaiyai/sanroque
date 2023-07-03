bbd9e8cffb4db0f2e05e6ecc2a4e8a66bc2940e9
yaiyai <yaiza.delrio@clupik.com>
2023-07-03 09:14:07 +0200

    wip vinos menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
   { path: 'carta', loadChildren: () => import('./components/menu/menu.module').then(m => m.MenuModule) },
+  { path: 'vinos', loadChildren: () => import('./components/menu/menu.module').then(m => m.MenuModule) },
   { path: 'menu-del-dia', loadChildren: () => import('./components/daymenu/daymenu.module').then(m => m.DaymenuModule) },
   { path: 'contacto', loadChildren: () => import('./components/contact/contact.module').then(m => m.ContactModule) },
 ];
