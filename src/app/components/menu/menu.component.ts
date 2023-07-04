4760f8afa4c2ce8772da557c56d4414d7082f16a
yaiyai <yaiza.delrio@clupik.com>
2023-07-04 18:08:47 +0200

    wine menu solved

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 import { ApiMenuService } from 'src/app/communication/api-menu.service';
 import { AppService } from 'src/app/communication/services/app/app.service';
 import { Dish } from 'src/app/models/dish.model';
@@ -12,7 +14,7 @@ export class MenuComponent {
   public all: Dish[] = [];
   public filtered: Dish[] = [];
   public isFilterOn: boolean = false;
-  public activeFilter: string = 'Ver todas';
+  public activeFilter: string = 'Ver todos';
   public categories: { order: number; name: string; }[] = [];
   public get dishes(): Dish[] {
     return this.isFilterOn ? this.filtered : this.all;
@@ -21,6 +23,7 @@ export class MenuComponent {
   constructor(
     private apiMenuService: ApiMenuService,
     private appService: AppService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -33,7 +36,12 @@ export class MenuComponent {
     this.apiMenuService.getCategories().subscribe({
       next: company => {
         this.categories = company.company[0].categories;
-        this.categories.unshift({ order: 0, name: 'Ver todas' });
+        if (this.router.url === '/vinos') {
+          this.categories = this.categories.filter(elm => elm.name === 'Cavas' || elm.name === 'Vinos Tintos' || elm.name === 'Vinos Blancos');
+        } else {
+          this.categories = this.categories.filter(elm => elm.name !== 'Cavas' && elm.name !== 'Vinos Tintos' && elm.name !== 'Vinos Blancos');
+        }
+        this.categories.unshift({ order: 0, name: 'Ver todos' });
         this.getAllDishes();
       },
       error: err => console.error(err)
@@ -45,6 +53,13 @@ export class MenuComponent {
     this.apiMenuService.dishes().subscribe({
       next: res => {
         this.all = res.dishes;
+        if (this.router.url === '/vinos') {
+          this.all = this.all.filter(elm => elm.category === 'Cavas' || elm.category === 'Vinos Tintos' || elm.category === 'Vinos Blancos' || elm.category === 'Vinos');
+        } else {
+          this.all = this.all.filter(elm => elm.category !== 'Cavas' && elm.category !== 'Vinos Tintos' && elm.category !== 'Vinos Blancos' && elm.category !== 'Vinos');
+        }
+        console.log(res.dishes);
+        console.log(this.all);
         this.filtered = this.dishes;
       },
       error: err => console.error(err)
@@ -58,7 +73,7 @@ export class MenuComponent {
 
   filter(type: string) {
     this.activeFilter = type;
-    if (type === 'Ver todas') {
+    if (type === 'Ver todos') {
       this.isFilterOn = false;
     } else {
       if (type === 'Vinos Tintos') {
