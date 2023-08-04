3ca5f36e625f50c1d04bb97aa1bd408df4b940ea
yaiyai <yaiza.delrio@clupik.com>
2023-08-04 09:46:45 +0200

    meta description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AppService } from './communication/services/app/app.service';
 import { ApiCompanyService } from './communication/api-company.service';
-
+import { Meta, Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,7 +14,19 @@ export class AppComponent {
   constructor(
     public appService: AppService,
     private apiCompanyService: ApiCompanyService,
-  ) { }
+    private meta: Meta,
+    private metaTitle: Title
+  ) {
+    this.meta.addTags([
+      { name: 'description', content: 'Restaurante de comida tradicional y caza' },
+      { name: 'author', content: 'https://mfi.es/' },
+      { name: 'keywords', content: 'Brañosera, Restaurante, 824, Jandrin, Caza, Menu del dia, Menu, Dia, Banquetes' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:url', content: 'https://www.sanroquebranosera.es/' },
+      { name: 'twitter:title', content: 'Restaurante San Roque || Brañosera' },
+      { name: 'twitter:description', content: 'Restaurante de comida tradicional y caza' },
+    ]);
+  }
 
   ngOnInit() {
     this.getCompany();
