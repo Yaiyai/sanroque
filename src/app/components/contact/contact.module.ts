0e5749cb38df9c0c4d8012b4c57dcc67379076bf
yaiyai <yaiza.delrio@clupik.com>
2023-10-23 18:58:11 +0200

    contact page translated

--- src/app/components/contact/contact.module.ts
+++ src/app/components/contact/contact.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ButtonModule } from 'src/app/_ui/button/button.module';
+import { SharedModule } from '../shared.module';
 @NgModule({
     imports: [
         CommonModule,
         ButtonModule,
+        SharedModule,
     ],
     declarations: [ContactComponent],
     exports: [ContactComponent, ContactRoutingModule]
